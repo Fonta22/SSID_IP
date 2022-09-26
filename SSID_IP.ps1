17c4bf7f62ebbc773c4bcfbf2252a1e4d841e244
Fonta22 <paufontasp@gmail.com>
2022-09-26 12:54:12 +0200

    CSV and gitignore

--- SSID_IP.ps1
+++ SSID_IP.ps1
@@ -5,15 +5,13 @@ netsh wlan export profile key=clear
 Compress-Archive -U -Path .\*.xml -DestinationPath $SSID_Zip
 rm .\*.xml
 
-(Invoke-WebRequest -uri "http://ifconfig.me/ip").Content | Out-File -FilePath .\Public.log
+$Public = (Invoke-WebRequest -uri "http://ifconfig.me/ip").Content
 
 $Private = (Get-NetIPAddress | Where-Object {$_.AddressState -eq "Preferred" -and $_.ValidLifetime -lt "24:00:00"}).IPAddress
-$Private | Out-File -FilePath .\Private.log
 
-[System.Net.Dns]::GetHostByAddress($Private).Hostname | Out-File -FilePath .\HostName.log
+$HostName = [System.Net.Dns]::GetHostByAddress($Private).Hostname
 
-Compress-Archive -U -Path .\*.log -DestinationPath $IP_Zip
-rm .\*.log
+"Host Name,$Hostname`nPublic IP,$Public`nPrivate IP,$Private" | Out-File -FilePath .\IP_Addresses.csv
 
-Write-Host "Saved IP Addresses to: $IP_Zip"
-Write-Host "Saved SSID Profiles to: $SSID_Zip"
+Write-Host "Saved IP Addresses to: IP_Addresses.csv"
+Write-Host "Saved SSID Profiles to: SSID_Profiles.zip"
