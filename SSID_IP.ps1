540440bfda564c102dd1db302098c83589db5625
Fonta22 <paufontasp@gmail.com>
2022-09-28 12:52:52 +0200

    Optimize SSID_IP.ps1

--- SSID_IP.ps1
+++ SSID_IP.ps1
@@ -1,6 +1,4 @@
 netsh wlan export profile key=clear
-Compress-Archive -U -Path .\Wi-Fi*.xml -DestinationPath SSID_Profiles.zip
-rm .\Wi-Fi*.xml
 
 $Public = (Invoke-WebRequest -uri "http://ifconfig.me/ip").Content
 $Private = (Get-NetIPAddress | Where-Object {$_.AddressState -eq "Preferred" -and $_.ValidLifetime -lt "24:00:00"}).IPAddress
@@ -8,5 +6,5 @@ $HostName = [System.Net.Dns]::GetHostByAddress($Private).Hostname
 
 "Host Name,Public IP,Private IP`n$HostName,$Public,$Private" | Out-File -FilePath .\IP_Addresses.csv
 
-Write-Host "Saved IP Addresses to: IP_Addresses.csv"
-Write-Host "Saved SSID Profiles to: SSID_Profiles.zip"
+Compress-Archive -U -Path .\Wi-Fi*.xml, IP_Addresses.csv -DestinationPath SSID_IP.zip
+rm .\Wi-Fi*.xml, IP_Addresses.csv
